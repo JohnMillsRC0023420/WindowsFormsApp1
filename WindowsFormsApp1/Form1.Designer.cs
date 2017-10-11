002dfe3b87c98f039772b26a30b5d20b905f53ec
Mills <rc0023420@student.riversidecollege.ac.uk>
2017-10-11 15:07:34 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button button1;
+            System.Windows.Forms.Button button2;
+            System.Windows.Forms.Button button3;
+            System.Windows.Forms.Button button4;
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(976, 546);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(83, 43);
+            button1.TabIndex = 1;
+            button1.Text = "Right";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.Bart_Simpson_Head__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(471, 269);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(108, 79);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(893, 595);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(83, 43);
+            button2.TabIndex = 2;
+            button2.Text = "Up";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            button3.Location = new System.Drawing.Point(815, 546);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(83, 43);
+            button3.TabIndex = 3;
+            button3.Text = "Left";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            button4.Location = new System.Drawing.Point(893, 497);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(83, 43);
+            button4.TabIndex = 4;
+            button4.Text = "Down";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 690);
+            this.Controls.Add(button4);
+            this.Controls.Add(button3);
+            this.Controls.Add(button2);
+            this.Controls.Add(button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
